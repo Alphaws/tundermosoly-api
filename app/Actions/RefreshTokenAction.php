1ff7a33cc599e6381aad8fdc313eb4cd44b32677
Szeness Imre <szeness.imre@devwing.hu>
2023-07-24 11:41:31 +0200

    jwt auth

---
+++ app/Actions/RefreshTokenAction.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Actions;
+
+use Lorisleiva\Actions\Concerns\AsAction;
+
+class RefreshTokenAction
+{
+    use AsAction;
+
+    public function handle()
+    {
+        // ...
+    }
+}
